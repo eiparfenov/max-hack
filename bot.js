6f96eedefbf480b4c77620da717c7b84146bb55d
eiparfenov <epar2002@outlook.com>
2025-11-15 11:24:09 +0300

    finish

--- bot.js
+++ bot.js
@@ -2,18 +2,24 @@ import { Bot } from '@maxhub/max-bot-api';
 
 
 const bot = new Bot(process.env.TOKEN);
-const base_api = process.ML_BASE_API;
-bot.on('message_created', async (ctx) => { 
-    const msg = await ctx.getMessage()
-    var response = fetch(`${base_api}/query`, {
-        method: "POST",
-        headers: {},
-        body: {
-            message: msg.body.text,
-            thread_id: ctx.chatId
-        }
-    })
-    const json = (await response).json()
-    ctx.reply(json.response);
+const base_api = process.env.ML_BASE_API;
+bot.on('message_created', (ctx) => {
+    const msg = ctx.message
+    const asf = async (msg) => {
+        var response = await fetch(`${base_api}/query`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify( {
+                "message": msg.body.text,
+                "thread_id": msg.recipient.chat_id.toString()
+                })
+            })
+        const json = await response.json()
+        console.log(msg.recipient.chat_id, json.response, json, msg.body.text, msg.body, msg)
+        bot.api.sendMessageToChat(msg.recipient.chat_id, json.response)
+    } 
+    asf(msg)
 })
 bot.start()
